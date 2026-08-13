e08bb33597086b7712a6c9b7f1fd3389b5cb4941
Ritesh Kumar <riteshsing628@gmail.com>
2026-08-13 15:30:56 +0530

    adding navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <>
     <div>
-      Hello world!
+      <Navbar/>
+      <img src="https://www.gif-vif.com/desi_gifs/kyu-hila-dala-na-rajinikanth-sivaji-the-boss-3da8c8d61b.gif"/>
+      <Footer/>
     </div>
     </>
   )
